244484834a2619c98f7a87e4e2420fc040bc878d
Isabella <isabella@bencardino.com.br>
2020-11-28 18:22:16 +0000

    add final project

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextfield.delegate = self
+        passwordTextfield.delegate = self
+    }
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.errorMessage.text = e.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                    self.errorMessage.text = ""
+                    
+                }
+            }
+        }
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField == emailTextfield) {
+           textField.text = ""
+        }
+        else if (textField == passwordTextfield) {
+            textField.text = ""
+        }
+    }
+    
     
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+         titleLabel.text = K.appName
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        super.viewDidDisappear(animated)
+        titleLabel.text = " "
+        
+    }
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
+        
+        
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+        
     }
+    
+    
     
 
 }
